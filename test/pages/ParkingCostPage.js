090cc5314617ae0e16ea0ed29d77a3e78e4d0c32
ilich-garcia <ilich-garcia@unitec.edu>
2021-03-12 19:48:13 -0600

    First general tests running

--- test/pages/ParkingCostPage.js
+++ test/pages/ParkingCostPage.js
@@ -4,6 +4,53 @@ class ParkingCostPage extends Page {
     open() {
         super.open('http://www.shino.de/parkcalc/');
     }
+
+    get parkingLot() {
+        return $('#ParkingLot');
+    }
+
+    get startingDate() {
+        return $('/html/body/form/table/tbody/tr[2]/td[2]/input[1]');
+    }
+
+    get leavingDate() {
+        return $('/html/body/form/table/tbody/tr[3]/td[2]/input[1]');
+    }
+
+    get startingTime() {
+        return $('#StartingTime');
+    }
+
+    get leavingTime() {
+        return $('#LeavingTime');
+    }
+
+    get estimatedParkingCostMessage() {
+        return $('/html/body/form/table/tbody/tr[4]/td[2]/b').getText();
+    }
+
+    get estimatedParkingCostResult() {
+        return $('/html/body/form/table/tbody/tr[4]/td[2]/span[1]/b').getText();
+    }
+
+    get calculateParkingCost() {
+        return $('/html/body/form/input[2]');
+    }
+
+    clearDates() {
+        this.startingDate.clearValue();
+        this.leavingDate.clearValue();
+    }
+
+    setSameDates() {
+        this.startingDate.addValue('3/8/2021');
+        this.leavingDate.addValue('3/8/2021');
+    }
+
+    clearLeavingDateAndTime() {
+        this.leavingTime.clearValue();
+        this.leavingDate.clearValue();
+    }
 }
 
 export default new ParkingCostPage();
--- test/specs/ParkingCost.js
+++ test/specs/ParkingCost.js
@@ -12,4 +12,31 @@ describe('Parking Cost Page', () => {
 
         title.should.be.equal('Parking Cost Calculator');
     });
+
+    it('should ask for a correctly formatted date', () => {
+        ParkingCostPage.calculateParkingCost.click();
+        browser.pause(2000);
+        
+        ParkingCostPage.estimatedParkingCostMessage.should.be.equal('ERROR! ENTER A CORRECTLY FORMATTED DATE');
+    });
+
+    it('should return $ 0.00', () => {
+        ParkingCostPage.clearDates();
+        browser.pause(2000);
+        ParkingCostPage.setSameDates();
+        browser.pause(2000);
+        ParkingCostPage.calculateParkingCost.click();
+        browser.pause(4000);
+
+        ParkingCostPage.estimatedParkingCostResult.should.be.equal('$ 0.00');
+    });
+
+    it('should maintain the selected parking lot', () => {
+        ParkingCostPage.parkingLot.selectByAttribute('value', 'Economy');
+        browser.pause(2000);
+        ParkingCostPage.calculateParkingCost.click();
+        browser.pause(4000);
+
+        chaiExpect(ParkingCostPage.parkingLot.getValue()).to.be.equal('Economy');
+    });
 });
